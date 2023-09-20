59b532277c75b69105896a40363d3a4a89fc0616
LuciaPuppo897 <alerome897@gmail.com>
2023-09-20 12:41:07 -0700

    fixed 6 and 12

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num in range(0, 10):
     for num2 in range(num + 1, 10):
         if num != 8:
-            print("{}{},".format((num, num2)), end='')
+            print("{}{},".format(num, num2), end='')
         else:
-            print("{}{},".format((num, num2)))
+            print("{}{},".format(num, num2))
