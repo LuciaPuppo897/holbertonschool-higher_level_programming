ef6e44ee42365f5a2382bb18b076f34c8af61ffe
LuciaPuppo897 <alerome897@gmail.com>
2023-09-20 11:51:45 -0700

    add taask 7 8 9 10 11

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_ =(((number * -1) % 10) * -1)
+    last =(((number * -1) % 10) * -1)
 else:
-    last_ = number % 10
-if last_ > 5:
-    print(f"Last digit of {number} is {last_} and is greater than 5")
-elif last_ < 6 and last_ != 0:
-    print(f"Last digit of {number} is {last_} and is less than 6 and not 0")
+    last = number % 10
+if last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+elif last < 6 and last != 0:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {last_} and is 0")
+    print(f"Last digit of {number} is {last} and is 0")
---
+++ python-if_else_loops_functions/8-isupper.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        char = ord(char)
+        if char > 96 and char < 123:
+            char = -32
+        print("{},".format((char)), end='')
