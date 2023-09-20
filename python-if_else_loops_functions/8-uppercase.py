77fc785dc3d1dc7c08c075c940c28f4bc5b0052a
LuciaPuppo897 <alerome897@gmail.com>
2023-09-20 12:49:28 -0700

    fiex task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for char in str:
         char = ord(char)
-        if char > 96 and char < 123:
-            char = -32
-        print("{},".format(chr(char)), end='')
+        if char >= ord('a') and char < ord('z'):
+            char -= 32
+        print("{}".format(chr(char)), end='')
     print()
