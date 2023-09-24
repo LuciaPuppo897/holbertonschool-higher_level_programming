7a0cb5e3487f247f6551632b1b2e59a5325e50de
LuciaPuppo897 <alerome897@gmail.com>
2023-09-24 02:09:09 -0700

    fixed 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-
+import sys
 if __name__ == "__main__":
-    num_args = len(argv) - 1
+    num_args = len(sys.argv) - 1 #punto llama a funcion
 
 if num_args == 0:
     print(" 0 arguments.")
@@ -11,5 +10,5 @@ else:
         print("1 argument:")
     else:
         print("{} arguments:".format(num_args))
-        for i, arg in enumerate(argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+        for i in range(1, num_args + 1):
+            print("{}: {}".format(i, sys.argv[i]))
