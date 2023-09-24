15242d023e67e2a266afc646437f1d4944842720
LuciaPuppo897 <alerome897@gmail.com>
2023-09-24 02:01:56 -0700

    fixed

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys 
-sum_of_args = 0
-
-for arg in range(len(sys.argv)-1):
-    sum_of_args += int(arg)
+import sys 
+if __name__ == "__main__":     
+    sum_of_args = 0
+for arg in range(len(sys.argv) -1):
+    sum_of_args += int(sys.argv[arg + 1])
 print("{}".format(sum_of_args))
