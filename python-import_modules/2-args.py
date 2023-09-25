6cb83f0cbbfda70db63cdac9fa87982bf6578eb6
LuciaPuppo897 <alerome897@gmail.com>
2023-09-24 19:49:19 -0700

    fixed pystyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1 #punto llama a funcion
+    num_args = len(sys.argv) - 1  # punto llama a funcion
 if num_args == 0:
     print("0 arguments.")
-elif num_args== 1:
+elif num_args == 1:
     print("1 argument:")
 else:
     print("{} arguments:".format(num_args))
