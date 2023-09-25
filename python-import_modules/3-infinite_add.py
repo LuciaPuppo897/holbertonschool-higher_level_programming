b7bcf5708bd473157fa1eb55519fd441e9231710
LuciaPuppo897 <alerome897@gmail.com>
2023-09-24 19:50:36 -0700

    fixed pystyle

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys 
-if __name__ == "__main__":     
+import sys
+if __name__ == "__main__":
     sum_of_args = 0
-for arg in range(len(sys.argv) -1):
+for arg in range(len(sys.argv) - 1):
     sum_of_args += int(sys.argv[arg + 1])
 print("{}".format(sum_of_args))
