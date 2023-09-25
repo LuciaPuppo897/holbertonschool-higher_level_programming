433b18671031f418f850cad626ee79a668ecf62f
LuciaPuppo897 <alerome897@gmail.com>
2023-09-24 19:47:32 -0700

    fix 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,11 @@
 import sys
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1 #punto llama a funcion
-
 if num_args == 0:
-    print(" 0 arguments.")
+    print("0 arguments.")
+elif num_args== 1:
+    print("1 argument:")
 else:
-    if num_args== 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(num_args))
-        for i in range(1, num_args + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+    print("{} arguments:".format(num_args))
+for i in range(1, num_args + 1):
+    print("{}: {}".format(i, sys.argv[i]))
