6f6a628c190e75e93d9647c516c9edd9f160bce0
LuciaPuppo897 <alerome897@gmail.com>
2023-09-28 11:36:54 -0700

    fixed 4 add task 5

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,6 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result_list.append(0)
             print("wrong type")
         except IndexError:
+            result_list.append(0)
             print("out of range")
         finally:
             pass
