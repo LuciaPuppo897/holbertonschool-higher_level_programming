2ccb0fc9c57d2cb6101d803fab890120ac0e2896
LuciaPuppo897 <alerome897@gmail.com>
2023-09-28 11:54:46 -0700

    fixed 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end=" ")
         except Exception:
             print()
-            return i + 1
+            return i
     print()
     return x
-
