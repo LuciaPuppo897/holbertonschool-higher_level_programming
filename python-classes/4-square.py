1580578ecfcf6d6030eeeee4c9c95c1196c4ff9f
LuciaPuppo897 <alerome897@gmail.com>
2023-09-30 12:19:02 -0700

    add task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A class that defines a Square"""
+
+
+class Square:
+    """Private attribute def size"""
+    def __init__(self, size):
+        self.__size = size
+
+    @property   # allows to modify size
+    def size(self):
+        """returns size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns area"""
+        return self.__size ** 2
