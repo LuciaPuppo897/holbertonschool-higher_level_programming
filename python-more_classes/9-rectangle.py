70a46f5e400ac84fdcf0a0c9ac23e974c788e665
LuciaPuppo897 <alerome897@gmail.com>
2023-10-04 09:57:03 -0700

    fixed

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
             rectangle_str = ""
             for _ in range(self.height):
                 rectangle_str += str(self.print_symbol) * self.width + "\n"
-            return rectangle_str[:-1]  # Remove the trailing newline
+            return rectangle_str[:-1]
 
     def __repr__(self):
         """Instance method that returns an official str rep of an instance"""
