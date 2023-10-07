81d2fddaf518097a2f6ff9cf8d1629c74652fcdb
LuciaPuppo897 <alerome897@gmail.com>
2023-10-07 09:56:48 -0700

    fixed error

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,6 @@
 """a function that returns true is the objects is an exact istance of an obj"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """return true if exact instance or inherantce from other"""
     return isinstance(obj, a_class)
