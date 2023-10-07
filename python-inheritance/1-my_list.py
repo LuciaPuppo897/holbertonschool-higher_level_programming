cd107b86bdd2e8d014f6289c5f60b9c5737c83e1
LuciaPuppo897 <alerome897@gmail.com>
2023-10-07 10:12:10 -0700

    fixed 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-"""class that inheritance from lookup"""
+"""class that inheritance from list"""
 
 
 class MyList(list):
     """Mylist class"""
-
     def print_sorted(self):
         """public method that sorts a list"""
-        sorted_list = sorted(self)
+        sorted_list = self.copy()
+        sorted_list.sort()
         print(sorted_list)
