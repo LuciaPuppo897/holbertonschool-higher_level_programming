0ad4438b0a970ede1671df15183a5ad1ed59e2be
LuciaPuppo897 <alerome897@gmail.com>
2023-10-08 09:20:15 -0700

    add task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""inherits from 5-base-geometry"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """public method"""
+        raise Exception("area() is not implemented")
