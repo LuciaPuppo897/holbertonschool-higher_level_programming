6f987f243e6a49a8a41d4c2bcfae4b4f3bcd6293
LuciaPuppo897 <alerome897@gmail.com>
2023-10-08 10:30:07 -0700

    add task 10 and 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Class that def a square with width and height as size"""
+"""Class that def a square with width and height as size and print a str"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -14,3 +14,7 @@ class Square(Rectangle):
     def area(self):
         """returns the area"""
         return self.__size ** 2
+
+    def __str__(self) -> str:
+        """returns an informal string"""
+        return f"[Square] {self.__size}/{self.__size}"
