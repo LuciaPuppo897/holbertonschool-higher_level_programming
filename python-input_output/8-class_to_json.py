69d97c343ee7c5b41e0b7742bb89ee2031404ae7
LuciaPuppo897 <alerome897@gmail.com>
2023-10-08 19:02:47 -0700

    fixed 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 def class_to_json(obj):
     """Check if obj is an instance of a class"""
-    if not hasattr(obj, '__dict__'):
-        raise ValueError("obj must be an instance of a class")
-
-    obj_dict = vars(obj)
-
-    json_dict = {}
-    for key, value in obj_dict.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            json_dict[key] = value
-
-    return json_dict
+    return repr({key: value for (key, value) in obj.__dict__.items()
+                if key in list(obj.__dict__.keys())})
