5d137624a9658e7535f501c74831fafd352bfa37
LuciaPuppo897 <alerome897@gmail.com>
2023-10-08 18:19:33 -0700

    fixed 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 def read_file(filename=""):
     """reads a file"""
-    with open(filename, encoding='utf-8') as myFile:
-        print(myFile.read(), end="")
+    with open(filename, 'r', encoding='utf-8') as myfile:
+        print(myfile.read(), end='')
+
