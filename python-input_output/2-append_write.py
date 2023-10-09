9ef8e45d9ead9adfbf961f517c52ade6b7b19fb0
LuciaPuppo897 <alerome897@gmail.com>
2023-10-08 18:36:44 -0700

    add task 4

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """function that append a string at the end of a file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """appends a string in a file"""
     with open(filename, 'a', encoding='utf-8') as myfile:
         return myfile.write(text)
--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """a function that returns the JSON rep of an object (string)"""
 
 
