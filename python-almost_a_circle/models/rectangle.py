064f31a594975659694e6c697fafeaed5180b1e2
LuciaPuppo897 <alerome897@gmail.com>
2023-10-10 20:55:38 -0700

    fixed rectangle update _init_ values to the correct ones

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,62 +9,66 @@ from .base import Base
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Call the super class constructor with id and initializate them"""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
         super().__init__(id)
+        self.__width = None
+        self.__height = None
+        self.__x = None
+        self.__y = None
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
-        """def priv attr width"""
+        """def width priv prop"""
         return self.__width
-
-    @property
-    def height(self):
-        """def priv attr heigth"""
-        return self.__height
-
-    @property
-    def x(self):
-        """def x"""
-        return self.__x
-
-    @property
-    def y(self):
-        """def y"""
-        return self.__y
 
     @width.setter
     def width(self, value):
-        """set width value validation"""
+        """set value attributes"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """def priv height prop"""
+        return self.__height
+
     @height.setter
     def height(self, value):
-        """set height value validator"""
+        """set value attributes"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
+    @property
+    def x(self):
+        """def priv x prop"""
+        return self.__x
+
     @x.setter
     def x(self, value):
-        """set x value validator"""
+        """set value attributes"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
+    @property
+    def y(self):
+        """def y priv prop"""
+        return self.__y
+
     @y.setter
     def y(self, value):
-        """set y value validator"""
+        """set value attributes"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
