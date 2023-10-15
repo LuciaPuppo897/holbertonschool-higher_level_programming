16b836e9995afc0628261951ee4e179190c4d249
LuciaPuppo897 <alerome897@gmail.com>
2023-10-15 07:59:06 -0700

    fixed task 9 by changing kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,20 +97,17 @@ class Rectangle(Base):
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
-                self.size = args[1]
+                self.width = args[1]
             if len(args) >= 3:
-                self.x = args[2]
+                self.height = args[2]
             if len(args) >= 4:
-                self.y = args[3]
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
         else:
-            if 'id' in kwargs:
-                self.id = kwargs['id']
-            if 'size' in kwargs:
-                self.size = kwargs['size']
-            if 'x' in kwargs:
-                self.x = kwargs['x']
-            if 'y' in kwargs:
-                self.y = kwargs['y']
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
 
     def to_dictionary(self):
         """Return a dictionary of the Rectangle"""
