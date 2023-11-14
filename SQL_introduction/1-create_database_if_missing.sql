487201f439a1a3a028dbbb3a38d0c4847cc9bcbe
LuciaPuppo897 <alerome897@gmail.com>
2023-11-14 07:34:25 -0800

    added task 2 fixed task 1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,2 @@
 -- creates a hbtn_0c_0 database without using select or show
-IF NOT EXISTS hbtn_0c_0
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- deletes a hbtn_0c_0 database without using select or show
+DROP DATABASE IF EXISTS hbtn_0c_0;
