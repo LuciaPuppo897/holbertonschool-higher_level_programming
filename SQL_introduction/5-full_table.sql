d7cc7b64fbe57761534191b6ad8c33210f7738a4
LuciaPuppo897 <alerome897@gmail.com>
2023-11-14 08:04:36 -0800

    fixed task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- Write a script that prints the following description of the table first_table
-DESC first_table;
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
