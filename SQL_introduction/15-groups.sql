b4bf8e5b0af9e954ede941c0b65c792891c9ef4e
LuciaPuppo897 <alerome897@gmail.com>
2023-11-16 15:37:03 -0800

    fixed 6

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 --Write a script that lists the number of records with the same score in the table second_table
 SELECT score,
-COUNT(*) as number 
+COUNT(*) AS 'number'
 FROM second_table
 GROUP BY score
 ORDER BY score DESC
