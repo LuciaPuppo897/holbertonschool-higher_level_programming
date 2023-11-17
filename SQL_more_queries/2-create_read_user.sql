5d70277db2b2fc5447e0e214a7cdf54961272bb9
LuciaPuppo897 <alerome897@gmail.com>
2023-11-16 19:32:35 -0800

    check 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 --Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- task 2
 CREATE DATEBASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost;
 SET PASSWORD FOR user_0d_2@localhost ='user_0d_2_pwd';
