fe873a6b129b4318b8a296fe155a85dc847b66cf
LuciaPuppo897 <alerome897@gmail.com>
2023-11-16 19:43:21 -0800

    checking 1 2 3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
---Write a script that creates the table force_name on your MySQL server.
+-- Write a script that creates the table force_name on your MySQL server.
 -- task 3
 CREATE TABLE IF IS NOT EXISTS force_name (
     id INT,
