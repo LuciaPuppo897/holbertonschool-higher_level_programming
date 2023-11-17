2b7efb948b11415c467795ed30613d1249f6cb73
LuciaPuppo897 <alerome897@gmail.com>
2023-11-17 06:55:06 -0800

    fixed 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+-- task 6
 CREATE DATEBASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXIST states(
