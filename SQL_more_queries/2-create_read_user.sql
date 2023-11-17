e5081e7b50ad6586dbb742303bcbb2b00b6acbeb
LuciaPuppo897 <alerome897@gmail.com>
2023-11-16 19:25:57 -0800

    check if works 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 --Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE DATEBASE IF NOT EXIST hbtn_0d_2;
-CREATE USER IF NOT EXIST 'user_0d_2'@'localhost';
+CREATE DATEBASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost;
 SET PASSWORD FOR user_0d_2@localhost ='user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
