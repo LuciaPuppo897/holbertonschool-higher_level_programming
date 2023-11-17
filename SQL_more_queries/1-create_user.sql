e502d7ad60200a62681f705acbc7ee61e4af3404
LuciaPuppo897 <alerome897@gmail.com>
2023-11-16 19:04:36 -0800

    refixed 1 n 0

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,7 @@
 --Write a script that creates the MySQL server user user_0d_1.
-CREATE USER
-        IF NOT EXIST user_0d_1@localhost 
+CREATE USER IF NOT EXIST user_0d_1@localhost;
         SET PASSWORD FOR user_0d_1@localhost ='user_0d_1_pwd';
 GRANT ALL PRIVILEGES
          ON *.* 
-         TO user_0d_1@localhost
-FLUSH PRIVILEGES
+         TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
