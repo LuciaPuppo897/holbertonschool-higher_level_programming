2429a5efdacad64feeafd842b19ef2993b7dbcbe
LuciaPuppo897 <alerome897@gmail.com>
2023-11-16 20:01:18 -0800

    add task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+CREATE DATEBASE IF NOT EXISTS hbtn_0d_usa
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXIST states (
+    id INT AUTO_INCREMENT PRIMARY KEY
+    name VARCHAR(256) NOT NULL
+);
