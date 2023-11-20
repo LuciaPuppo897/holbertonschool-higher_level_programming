244b058a9b03caa557a25ab1f4c4a4622e72e402
LuciaPuppo897 <alerome897@gmail.com>
2023-11-19 16:05:10 -0800

    fixed

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 -- task 10
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows ts
-JOIN tv_show_genres tvg ON ts.id = tvg.show_id
-ORDER BY ts.title ASC, tvg.genre_id ASC;
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- task 12
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows ts
-LEFT JOIN tv_show_genres tvg ON tv_shows.id = tv_show_genres.show_id
-WHERE tvg.genre_id IS NULL
-ORDER BY ts.title ASC, tvg.genre_id ASC;
+FROM tv_shows 
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
