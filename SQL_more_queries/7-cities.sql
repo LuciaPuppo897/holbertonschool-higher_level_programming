3a4a701a72c03013a9c884029dcdc5518b5b0aef
LuciaPuppo897 <alerome897@gmail.com>
2023-11-19 16:38:47 -0800

    correct typo

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,8 @@
 -- Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa
 -- Task 7
-CREATE DATEBASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
-);
+    id  INT AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
+    state_id    INT NOT NULL,
+    name    VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
