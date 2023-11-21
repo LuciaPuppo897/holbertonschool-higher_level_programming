03edec75f6c2df5adf78166e0f88e4808115f797
LuciaPuppo897 <alerome897@gmail.com>
2023-11-21 12:36:32 -0800

    add task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a class student that defines a student"""
+
+
+class Student():
+    """def a new class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializate the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictonary rep of the student with specifications """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
