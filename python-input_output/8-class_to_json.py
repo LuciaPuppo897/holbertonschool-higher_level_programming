7d3d7b6c5efe931f8f2e6db9d59ba3131d54b950
LuciaPuppo897 <alerome897@gmail.com>
2023-11-21 13:05:48 -0800

    fixed 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""def a method that returns a json dict"""
+
+
 def class_to_json(obj):
-    """Check if obj is an instance of a class"""
-    return repr({key: value for (key, value) in obj.__dict__.items()
-                if key in list(obj.__dict__.keys())})
+    """return a json rep of an object"""
+    return obj.__dict__
